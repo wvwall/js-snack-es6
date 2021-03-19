87aa1fe56ceba2eb47ca3bcff1a18dec0e38ef9f
wvwall <waltervelardo@gmail.com>
2021-03-19 16:35:47 +0100

    Milestone 2

--- README.md
+++ README.md
@@ -14,4 +14,4 @@ Milestone 2
 Dividere i gatti in due contenitori distinti in base al sesso e aggiungere a fianco di ogni gattino un fiocco colorato di rosa, se femmina, o di blu, se maschio. Il colore del fiocco deve essere più tenue se il gatto è più giovane, più scuro se il gatto è più vecchio.
 
 Milestone 3
-Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome e colore e colore e opacità del fiocco per ogni gatto.
+Creare un nuovo array con prima tutti i gattini femmina e poi tutti i gattini maschio, inserendo solamente nome colore e opacità del fiocco per ogni gatto.
--- cats/main.js
+++ cats/main.js
@@ -1,4 +1,6 @@
 $(document).ready(function () {
+
+    //MILESTONE 1
     
 // Definire array oggetti gatti {nome età colore e sesso}
 
@@ -6,7 +8,7 @@ $(document).ready(function () {
 
         {
             nome : "Virgola",
-            eta : 10,
+            eta : 8,
             colore : "Bianco",
             sesso : "F"
         },
@@ -38,8 +40,44 @@ $(document).ready(function () {
     //Ciclo con for each e stampo proprietà nome e colore
 
     cats.forEach((element,i,array) => {
-        console.log(` Il gatto  ${element.nome} è di colore ${element.colore}`);
-        $("#cats").append(`<li> Il gatto  ${element.nome} è di colore ${element.colore} </li>`);
+        //console.log(` Il gatto  ${element.nome} è di colore ${element.colore}`);
+        $("#cats1").append(`<li> Il gatto  ${element.nome} è di colore ${element.colore} </li>`);
     });
+
+
+    //MILESTONE 2
+
+    //Dividere i gatti in due array in base al sesso con filter
+
+    //primo metodo
+
+    /* const catsM = cats.filter((element) => element.sesso == "M" );
+    //console.log(catsM);
+    
+    catsM.forEach((element) => {
+        $("#cats2").append(`<li class = "azzurro"> ${element.nome}  è di colore ${element.colore} <i class="fas fa-ribbon"></i> </li> `);
+    });
+    
+
+    const catsF = cats.filter((element) => element.sesso == "F" );
+    //console.log(catsF);
+
+    catsF.forEach((element) => {
+        $("#cats2").append(`<li class = "rosa"> ${element.nome} è di colore ${element.colore} <i class="fas fa-ribbon"></i> </li> `);
+    }); */
+
+    // Metodo più flessibile
+
+    cats.forEach((element) => {
+        let colore = "azzurro";
+        if(element.sesso == "M") {
+            $("#cats2").append(`<li class = "${colore}"> ${element.nome}  è di colore ${element.colore} <i class="fas fa-ribbon"></i> </li> `);
+        } else {
+            let colore = "rosa";
+            $("#cats2").append(`<li class = "${colore}"> ${element.nome}  è di colore ${element.colore} <i class="fas fa-ribbon"></i> </li> `);
+        }
+       
+    });
+
 
 });
